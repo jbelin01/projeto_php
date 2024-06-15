31517072169c66bbfa40e073890ec8540a8fc12b
jbelin01 <joao_gabriel.silva2001@hotmail.com>
2024-06-15 20:14:27 -0300

    esqueci minha senha

--- FormCadastro.php
+++ FormCadastro.php
@@ -1,22 +1,12 @@
-<form action="Login.php" method="post">
-    <h1>CADASTRO</h1>
-    <div>
-        <label for="username">usuario:</label>
-        <input type="text" id="username" name="username" required>
-    </div>
-
-
-    <div>
-        <label for="senha">Senha:</label>
-        <input type="password" id="senha" name="senha" required>
-    </div>
-
-    <div>
-        <label for="confirmar">Conrfime sua senha:</label>
-        <input type="password" id="confirmar" name="confirmar" required>
-    </div>
-
-    <div class="enviar">
-        <input type="submit" value="Enviar"></input>
-    </div>
+<form method="POST" action="">
+    <label for="username">Usuário:</label>
+    <input type="text" id="username" name="username" required>
+    <br>
+    <label for="senha">Senha:</label>
+    <input type="password" id="senha" name="senha" required>
+    <br>
+    <label for="confirmar_senha">Confirmar Senha:</label>
+    <input type="password" id="confirmar_senha" name="confirmar_senha" required>
+    <br>
+    <input type="submit" value="Cadastrar">
 </form>
